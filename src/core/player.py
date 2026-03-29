a56404ba42726eb67feffecae1f6df8e484092db
alengyel <alengyel@adobe.com>
2026-03-29 19:49:44 +0200

    implement db for stats

--- src/core/player.py
+++ src/core/player.py
@@ -1,12 +1,12 @@
-from utils.app_types import TBoard, TCoord, TShipCoords, TShips
+from utils.app_types import TCoord, TShipCoords
 from .board import Board
 from .ships import Ships
 
 class Player:
     def __init__(self, name: str):
-        self.name: str = name
-        self.board: TBoard = Board()
-        self.ships: TShips = Ships()
+        self.player = self._get_or_create_player(name)
+        self.board: Board = Board()
+        self.ships: Ships = Ships()
 
     def place_ship(self, ship_name: str, ship_coords: TShipCoords) -> None:
         self.ships.add_ship(ship_name, ship_coords)
@@ -18,4 +18,23 @@ class Player:
         if ship:
             player.ships.decrease_ship(ship, coord)
 
-        print(f"{self.name.capitalize()} shot at \"{coord[0]} {coord[1]}\" and {'hit a ship!' if ship else 'missed.'}")
+        print(f"{self.player.name.capitalize()} shot at \"{coord[0]} {coord[1]}\" and {'hit a ship!' if ship else 'missed.'}")
+
+    def save_match(self, number_of_rounds: int, has_player_won: bool):
+        from src.stats import match as stats_match
+        return stats_match.create_match(
+            player_id = self.player.id,
+            number_of_rounds = number_of_rounds,
+            has_player_won = has_player_won
+        )
+
+    def _get_or_create_player(self, name: str):
+        from src.stats import player
+
+        db_player = player.get_player_by_name(name)
+        if db_player:
+            return db_player
+        
+        return player.create_player(name)
+    
+    
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,23 +1,11 @@
 from types import SimpleNamespace
 
 MENU_MESSAGE = "                    /\\_/\\ \n                     ( o.o )\n                      > ^ < \n     ______________________________________________________\n    /   ~  ~  ~  ~  ~  ~  BATTLESHIP  ~  ~  ~  ~  ~  ~    |=====[]>\n __/______________________________________________________|______\n<_______________________________________________________________/\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-STATS_TITLE_ART = "     /\\_/\\  \n      ( o.o ) \n       > ^ <  \n  +--------------------+\n  |       STATS        |\n  +--------------------+"
-STATS_MESSAGE = STATS_TITLE_ART + "\nNo stats available. Please create a profile first or load the data from a file."
 
 # AFTER HE LEFT THE GAME section with cat
 END_MESSAGE = "       /\\_/\\  \n      ( o.- )  \n       > ^ <  \n      Goodbye!"
 
 CONFIG_MESSAGE = "Back to Main Menu"
-STATS_VIEW_MESSAGE = "Back to Stats Menu"
-
-
-STATS_PRINT_MESSAGE= SimpleNamespace(
-    usernNameInput= "Enter your username: ",
-    successSaveMessage= "Stats saved successfully.",
-    successLoadMessage= "Stats successfully loaded from file.",
-    successCreateMessage= "Profile created successfully.",
-    fileNameInput= "Enter the filename to load from (including path): ",
-)
 
 # Menu choices as constants
 MAIN_MENU_CHOICES = SimpleNamespace(
@@ -25,23 +13,6 @@ MAIN_MENU_CHOICES = SimpleNamespace(
     stats= "Stats",
     config= "Config",
     exit= "Exit"
-)
-
-STATS_MENU_CHOICES_NEW = SimpleNamespace(
-    create= "Create New Profile",
-    load= "Load from File",
-    back= "Back to Main Menu"
-)
-
-STATS_MENU_CHOICES = SimpleNamespace(
-    view= "View Stats",
-    save= "Save Stats",
-    load= "Load Stats from File",
-    back= "Back to Main Menu"
-)
-
-STATS_MENU_CHOICES_VIEW = SimpleNamespace(
-    back= "Press Enter"
 )
 
 CONFIG_MENU_CHOICES = SimpleNamespace(
