c4e5e9b5e325593d786365cfc73256df7cd13da3
CH-Niels <it@niels-meyer.com>
2026-05-20 10:03:39 +0200

    Final Fixes Niels - UI + Readme

--- src/ui/pages/player_selector_page.py
+++ src/ui/pages/player_selector_page.py
@@ -15,7 +15,7 @@ SELECTOR_TITLE_CLASS = TITLE_CLASS
 SELECTOR_HEADING_CLASS = SECTION_HEADING_CLASS
 
 # Value Constants
-SELECT_INPUT_PROPS = "use-input fill-input hide-selected input-debounce=0 new-value-mode=add-unique clearable"
+PLAYER_INPUT_PROPS = "clearable"
 
 @ui.page(ROOT_ROUTE)
 def player_selector_page() -> Any:
@@ -25,17 +25,19 @@ def player_selector_page() -> Any:
 
     player_name_options = get_player_name_options()
 
+    def continue_with_player() -> None:
+        select_or_create_player(player_input.value)
+
     with ui.column().classes(PAGE_CLASS):
         ui.label(APP_TITLE).classes(SELECTOR_TITLE_CLASS)
         with ui.card().classes(CARD_MEDIUM_CLASS):
             ui.label(PLAYER_SELECTOR_HEADING).classes(SELECTOR_HEADING_CLASS)
             ui.label(PLAYER_SELECTOR_HELP_TEXT)
-            player_select = ui.select(
-                options=player_name_options,
+            player_input = ui.input(
                 label=PLAYER_SELECTOR_INPUT_LABEL,
-                with_input=True,
-            ).props(SELECT_INPUT_PROPS).classes(FULL_WIDTH_CLASS)
+                autocomplete=player_name_options,
+            ).props(PLAYER_INPUT_PROPS).on("keydown.enter", continue_with_player).classes(FULL_WIDTH_CLASS)
             ui.button(
                 CONTINUE_LABEL,
-                on_click=lambda: select_or_create_player(player_select.value),
+                on_click=continue_with_player,
             ).classes(FULL_WIDTH_CLASS)
